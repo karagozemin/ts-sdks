0f093d5146b5fcc8cdaf32bb93392d168749abbe
William Robertson <williamrobertson@mystenlabs.com>
2025-02-23 21:38:29 -0500

    e

--- packages/walrus/src/utils/randomness.ts
+++ packages/walrus/src/utils/randomness.ts
@@ -6,31 +6,38 @@ type WeightedItem<T> = {
 	weight: number;
 };
 
-export function weightedShuffle<T>(array: WeightedItem<T>[]): T[] {
-	const result: T[] = [];
-	const remainingItems = [...array];
+export function weightedShuffle<T>(arr: WeightedItem<T>[]): T[] {
+	const totalWeight = arr.reduce((sum, element) => sum + element.weight, 0);
 
-	for (let i = 0; i < array.length; i += 1) {
-		let totalWeight = remainingItems.reduce((sum, { weight }) => sum + weight, 0);
-		let randomValue = Math.random() * totalWeight;
+	function getRandomWeightedIndex() {
+		const randomWeight = Math.random() * totalWeight;
 		let cumulativeWeight = 0;
 
-		for (let j = 0; j < remainingItems.length; j) {
-			const { value, weight } = remainingItems[j];
-			cumulativeWeight += weight;
-
-			if (randomValue <= cumulativeWeight) {
-				result.push(value);
-				remainingItems.splice(j, 1);
-				break;
+		for (let i = 0; i < arr.length; i += 1) {
+			cumulativeWeight += arr[i].weight;
+			if (randomWeight <= cumulativeWeight) {
+				return i;
 			}
+		}
+
+		return arr.length - 1;
+	}
+
+	const selectedIndexes = new Set<number>();
+	const result: T[] = [];
+
+	while (result.length < arr.length) {
+		const index = getRandomWeightedIndex();
+		if (!selectedIndexes.has(index)) {
+			selectedIndexes.add(index);
+			result.push(arr[index].value);
 		}
 	}
 
 	return result;
 }
 
-export function shuffle<T>(arr: readonly T[]): T[] {
+export function shuffle<T>(arr: T[]): T[] {
 	const result = [...arr];
 
 	for (let i = result.length - 1; i > 0; i -= 1) {
