fc6400441832889553d1162732d012d6b334d836
William Robertson <williamrobertson@mystenlabs.com>
2025-02-23 21:24:57 -0500

    fix

--- packages/walrus/src/utils/randomness.ts
+++ packages/walrus/src/utils/randomness.ts
@@ -1,6 +1,35 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+type WeightedItem<T> = {
+	value: T;
+	weight: number;
+};
+
+export function weightedShuffle<T>(array: WeightedItem<T>[]): T[] {
+	const result: T[] = [];
+	const remainingItems = [...array];
+
+	for (let i = 0; i < array.length; i += 1) {
+		let totalWeight = remainingItems.reduce((sum, { weight }) => sum + weight, 0);
+		let randomValue = Math.random() * totalWeight;
+		let cumulativeWeight = 0;
+
+		for (let j = 0; j < remainingItems.length; j) {
+			const { value, weight } = remainingItems[j];
+			cumulativeWeight += weight;
+
+			if (randomValue <= cumulativeWeight) {
+				result.push(value);
+				remainingItems.splice(j, 1);
+				break;
+			}
+		}
+	}
+
+	return result;
+}
+
 export function shuffle<T>(arr: readonly T[]): T[] {
 	const result = [...arr];
 
