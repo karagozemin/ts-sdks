9ee61751b5cf34cba3ee5b3f5e3a668077fab8f9
Clay Kohut <clay.kohut@mystenlabs.com>
2025-03-28 14:33:21 -0400

    working

--- packages/zksend/src/experimental/wallet/channel/events.ts
+++ packages/zksend/src/experimental/wallet/channel/events.ts
@@ -53,7 +53,7 @@ export const StashedResponseData = variant('type', [
 		accounts: array(
 			object({
 				address: string('`address` is required'),
-				publicKey: optional(instance(Uint8Array, '`publicKey` must be a Uint8Array')),
+				publicKey: optional(string(), '`publicKey` must be a string'),
 			}),
 		),
 		session: string('`session` is required'),
