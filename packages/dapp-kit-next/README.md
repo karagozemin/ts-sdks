8dac720bb24e2480cded705793c7fe33f838f5e3
William Robertson <williamrobertson@mystenlabs.com>
2025-04-10 12:39:39 -0400

    work

---
+++ packages/dapp-kit-next/README.md
@@ -0,0 +1,4 @@
+# dapp-kit-next
+
+> [!NOTE]
+> This package is highly experimental and subject to change.
---
+++ packages/dapp-kit-next/src/components/internal/wallet-list.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+</script>
+
+<ul class="wallet-list">
+	<ul>abc</ul>
+	<ul>def</ul>
+</ul>
