2ae11bdeb934613aae1bc03a3d6d6d98e37207d1
Clay Kohut <clay.kohut@mystenlabs.com>
2025-04-18 13:23:08 -0400

    working

--- packages/slush-wallet/src/index.ts
+++ packages/slush-wallet/src/index.ts
@@ -1,4 +1,4 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-export { registerStashedWallet, StashedWallet, STASHED_WALLET_NAME } from './wallet/index.js';
+export { registerSlushWallet, SlushWallet, SLUSH_WALLET_NAME } from './wallet/index.js';
