0366cba97500f84bc98f4825ed05a4f0cec1d7db
William Robertson <williamrobertson@mystenlabs.com>
2025-04-22 13:36:18 -0400

    oopsie

---
+++ packages/dapp-kit-next/src/components/dapp-kit-connect-modal.stories.ts
@@ -0,0 +1,18 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import type { Meta, StoryObj } from '@storybook/web-components';
+
+import '../components/dapp-kit-connect-modal.js';
+import { html } from 'lit';
+
+const meta = {
+	title: 'Connect Modal',
+	component: 'dapp-kit-connect-modal',
+	render: () => html`<dapp-kit-connect-modal></dapp-kit-connect-modal>`,
+	tags: ['autodocs'],
+} satisfies Meta<any>;
+
+export default meta;
+
+export const Default: StoryObj = {};
