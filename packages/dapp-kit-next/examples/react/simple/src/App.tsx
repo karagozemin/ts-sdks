f8531d856e5300277844403eb69d6c0c29f6f0ac
William Robertson <williamrobertson@mystenlabs.com>
2025-06-07 17:17:18 -0400

    fix

--- packages/dapp-kit-next/examples/react/simple/src/App.tsx
+++ packages/dapp-kit-next/examples/react/simple/src/App.tsx
@@ -1,32 +1,10 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { ConnectButton, useCurrentNetwork, useWallets } from '@mysten/dapp-kit-react';
-import { dAppKit } from './dApp-kit.js';
+import { ConnectButton } from '@mysten/dapp-kit-react';
 
 function App() {
-	// dAppKit.switchNetwork({ network: 'mainnet' });
-	const wallets = useWallets(dAppKit);
-	console.log(wallets);
-
-	// Correct
-	const currentNetwork2 = useCurrentNetwork({ dAppKit });
-	console.log(currentNetwork2);
-
-	// Correct
-	const currentNetwork = useCurrentNetwork();
-	console.log(currentNetwork);
-
-	// correct
-	// const _correct = dAppKit.stores.$currentNetwork.get();
-	// type _alsoCorrect = ResolvedRegister['dAppKit']['stores']['$currentNetwork'];
-
-	return (
-		<div>
-			<ConnectButton instance={dAppKit} />
-			<ConnectButton />
-		</div>
-	);
+	return <ConnectButton />;
 }
 
 export default App;
--- packages/dapp-kit-next/examples/react/simple/src/main.tsx
+++ packages/dapp-kit-next/examples/react/simple/src/main.tsx
@@ -3,11 +3,11 @@
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { DAppKitProvider } from '@mysten/dapp-kit-react';
+import { dAppKit } from './dApp-kit.ts';
 import App from './App.tsx';
 
 import './index.css';
-import { DAppKitProvider } from '@mysten/dapp-kit-react';
-import { dAppKit } from './dApp-kit.ts';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
