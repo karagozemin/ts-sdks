a6e5cb12e5189f52a5b813b305999871b96b67a2
William Robertson <williamrobertson@mystenlabs.com>
2025-06-08 14:14:55 -0400

    fix

--- packages/dapp-kit-next/examples/react/simple/src/dApp-kit.ts
+++ packages/dapp-kit-next/examples/react/simple/src/dApp-kit.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { createDAppKit, DAppKit, ResolvedRegister } from '@mysten/dapp-kit-next';
+import { createDAppKit } from '@mysten/dapp-kit-next';
 import { getFullnodeUrl, SuiClient } from '@mysten/sui/client';
 
 export const dAppKit = createDAppKit({
@@ -17,10 +17,3 @@ declare module '@mysten/dapp-kit-next' {
 		dAppKit: typeof dAppKit;
 	}
 }
-
-const b: DAppKit = dAppKit;
-console.log(b);
-
-dAppKit.getClient('');
-
-type D = ResolvedRegister['dAppKit'];
