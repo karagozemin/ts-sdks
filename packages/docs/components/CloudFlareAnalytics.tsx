d93c3f5c6089425716195d2985b8e75e11245054
Daniel Leavitt <daniel.leavitt@mystenlabs.com>
2025-06-10 13:15:36 -0700

    removed unnecessart fragment

--- packages/docs/components/CloudFlareAnalytics.tsx
+++ packages/docs/components/CloudFlareAnalytics.tsx
@@ -6,12 +6,10 @@ import Script from 'next/script';
 
 export default function CloudFlareAnalytics() {
 	return (
-		<>
-			<Script
-				defer
-				src="https://static.cloudflareinsights.com/beacon.min.js"
-				data-cf-beacon='{"token": "e5b9b395eb1e48a4b003705000860bb8"}'
-			/>
-		</>
+		<Script
+			defer
+			src="https://static.cloudflareinsights.com/beacon.min.js"
+			data-cf-beacon='{"token": "e5b9b395eb1e48a4b003705000860bb8"}'
+		/>
 	);
 }
