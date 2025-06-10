fee7aca03168f9babb2e22971bb30aecb484c534
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2025-06-10 10:44:36 +0200

    test vector

--- packages/seal/test/unit/shamir.test.ts
+++ packages/seal/test/unit/shamir.test.ts
@@ -4,6 +4,7 @@
 import { describe, expect, it } from 'vitest';
 
 import { combine, GF256, interpolate, Polynomial, split } from '../../src/shamir';
+import { fromHex } from '@mysten/bcs';
 
 describe('Shamir\'s secret sharing', () => {
 	it('secret sharing roundtrip', () => {
@@ -33,5 +34,15 @@ describe('Shamir\'s secret sharing', () => {
 		coordinates.forEach(({ x, y }) => {
 			expect(interpolated.evaluate(new GF256(x))).toEqual(new GF256(y));
 		});
+	});
+
+	it('test vector', () => {
+		const shares = [
+			{ index: 0x2d, share: fromHex("0bbad0cd0d222fe2fe7c17080198a95c186d66c523bbba") },
+			{ index: 0x59, share: fromHex("94ed01a1e8e883b0110555e38cb50ca8014ffc8badd19a") },
+			{ index: 0x3d, share: fromHex("16cad5ae8279f9e59fc3bb05817abc6370160cdd8e82f7") },
+		];
+		const combined = combine(shares);
+		expect(combined).toEqual(new TextEncoder().encode("My super secret message"));
 	});
 });
