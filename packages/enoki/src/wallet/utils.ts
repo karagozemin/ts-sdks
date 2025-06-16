342d4fe04b2f6fa93d096e1645dd2d99c11f9a3b
William Robertson <williamrobertson@mystenlabs.com>
2025-06-16 10:53:57 -0400

    make everything backward ocmpatible

--- packages/enoki/src/wallet/utils.ts
+++ packages/enoki/src/wallet/utils.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import type { Wallet } from '@mysten/wallet-standard';
+import type { IdentifierArray, Wallet, WalletWithFeatures } from '@mysten/wallet-standard';
 import type { UiWallet } from '@wallet-standard/ui';
 import { getWalletFeature } from '@wallet-standard/ui';
 import type { EnokiWallet } from './index.js';
@@ -11,33 +11,46 @@ import { EnokiGetMetadata } from './feature.js';
 export function isEnokiWallet(wallet: UiWallet): boolean;
 export function isEnokiWallet(wallet: Wallet): wallet is EnokiWallet;
 export function isEnokiWallet(wallet: Wallet | UiWallet) {
-	if (Array.isArray(wallet.features)) {
-		return wallet.features.includes(EnokiGetMetadata);
+	if (isWalletHandle(wallet)) {
+		const features = wallet.features as IdentifierArray;
+		return features.includes(EnokiGetMetadata);
 	}
 	return EnokiGetMetadata in wallet.features;
 }
 
-export function getWalletMetadata(enokiWallet: UiWallet) {
-	try {
-		const { getMetadata } = getWalletFeature(
-			enokiWallet,
-			EnokiGetMetadata,
-		) as EnokiGetMetadataFeature[typeof EnokiGetMetadata];
+export function getWalletMetadata(enokiWallet: Wallet | UiWallet) {
+	if (isWalletHandle(enokiWallet)) {
+		try {
+			const { getMetadata } = getWalletFeature(
+				enokiWallet,
+				EnokiGetMetadata,
+			) as EnokiGetMetadataFeature[typeof EnokiGetMetadata];
 
-		return getMetadata();
-	} catch (error) {
-		return null;
+			return getMetadata();
+		} catch (error) {
+			return null;
+		}
+	} else if (EnokiGetMetadata in enokiWallet.features) {
+		const walletWithFeature = enokiWallet as WalletWithFeatures<EnokiGetMetadataFeature>;
+		return walletWithFeature.features[EnokiGetMetadata].getMetadata();
 	}
+	return null;
 }
 
-export function isGoogleWallet(wallet: UiWallet) {
+export function isGoogleWallet(wallet: Wallet | UiWallet) {
 	return getWalletMetadata(wallet)?.provider === 'google';
 }
 
-export function isTwitchWallet(wallet: UiWallet) {
+export function isTwitchWallet(wallet: Wallet | UiWallet) {
 	return getWalletMetadata(wallet)?.provider === 'twitch';
 }
 
-export function isFacebookWallet(wallet: UiWallet) {
+export function isFacebookWallet(wallet: Wallet | UiWallet) {
 	return getWalletMetadata(wallet)?.provider === 'facebook';
+}
+
+function isWalletHandle(wallet: UiWallet | Wallet): wallet is UiWallet {
+	// TypeScript doesn't properly narrow readonly arrays:
+	// https://github.com/microsoft/TypeScript/issues/1700
+	return Array.isArray(wallet.features);
 }
