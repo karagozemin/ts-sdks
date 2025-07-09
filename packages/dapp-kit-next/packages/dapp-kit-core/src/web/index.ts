3f62a54d820eeb6c71ff48095a119a82b41db69d
William Robertson <williamrobertson@mystenlabs.com>
2025-07-09 00:42:53 -0400

    f

--- packages/dapp-kit-next/packages/dapp-kit-core/src/web/index.ts
+++ packages/dapp-kit-next/packages/dapp-kit-core/src/web/index.ts
@@ -3,8 +3,8 @@
 
 import '@webcomponents/scoped-custom-element-registry';
 
-import './ui/dapp-kit-connect-button.js';
-import './ui/dapp-kit-connect-modal.js';
+import './dapp-kit-connect-button.js';
+import './dapp-kit-connect-modal.js';
 
 export { DAppKitConnectButton } from './dapp-kit-connect-button.js';
 export { DAppKitConnectModal } from './dapp-kit-connect-modal.js';
